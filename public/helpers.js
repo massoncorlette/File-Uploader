410ed0cab8d5ff60dbddabda6b6bea820a2ffc00
massoncorlette <massoncorlette07@gmail.com>
2025-06-11 23:47:11 -0400

    feat: setting up my public file for client side rendering

---
+++ public/helpers.js
@@ -0,0 +1,9 @@
+
+
+export function openModal() {
+  document.getElementById("addFileModal").style.display = "block";
+};
+
+export function closeModal() {
+  document.getElementById("closeModalBtn").style.display = "none";
+};
