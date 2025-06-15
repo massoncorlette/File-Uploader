44a7cd9712a839441dd1230ab13684f5b10b3edd
massoncorlette <massoncorlette07@gmail.com>
2025-06-15 05:33:28 -0400

    feat(public/helpers): refactor to add click event for each modal, inside event all modal display none then using button id display block, adding type to form buttons to keep them wrapped in forms

--- public/helpers.js
+++ public/helpers.js
@@ -3,14 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
   function handleModal() {
 
     const toggleButtons = document.querySelectorAll(".addFileModal");
+    const modals = document.querySelectorAll(".homeModal");
     let addFileModal = document.getElementById("fileModal");
+    let addFolderModal = document.getElementById("folderModal");
+    let trashModal = document.getElementById("deleteModal");
 
     toggleButtons.forEach(button => {
-      button.addEventListener("click", () => {
-        if (addFileModal.style.display === "none" || addFileModal.style.display === "") {
+      button.addEventListener("click", function() {
+        modals.forEach(modal => {
+          modal.style.display = "none";
+        });
+
+        if (this.id === "uploadBtn") {
           addFileModal.style.display = "block";
-        } else {
-          addFileModal.style.display = "none";
+        } else if (this.id === "createBtn") {
+          addFolderModal.style.display = "block";
+        } else if (this.id === "deleteBtn") {
+          trashModal.style.display = "block";
         }
       });
     });
