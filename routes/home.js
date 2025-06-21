fde6f917485cf9479ffc470fdba4399bd5af1574
massoncorlette <massoncorlette07@gmail.com>
2025-06-20 23:52:21 -0400

    feat: refactor my multer middleware to store files to disk for now before implementing cloud logic

--- routes/home.js
+++ routes/home.js
@@ -5,8 +5,18 @@ const { handleCreateFolder } = require("../controllers/dataController/createCont
 const { validateCreateFolder } = require("../controllers/validation");
 
 const multer  = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+const upload = multer({ storage: storage });
+
 
 let uploadedFiles = [];
 
@@ -19,7 +29,7 @@ homeRouter.get("/", (req, res, next) => {
 
 homeRouter.post('/upload', upload.single('uploaded_file'), function (req, res) {
 
-  console.log(uploadedFiles);
+  console.log(req.file, req.body)
 
   uploadedFiles.push({
     originalname: req.file.originalname, 
