5cd2d967d5ef4e35a0eaf96d8d195af295d587c4
massoncorlette <massoncorlette07@gmail.com>
2025-06-26 00:38:42 -0400

    feat(config/cloud): adding cloudinary api config and env values

---
+++ config/cloud.js
@@ -0,0 +1,29 @@
+const cloudinary = require('cloudinary').v2;
+
+
+cloudinary.config({
+  cloud_name: 'dlcev9lgh',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+
+});
+
+async function getCloudinaryObj(path) {
+  const results = await cloudinary.uploader.upload(path);
+  console.log(results);
+  const url = cloudinary.url(results.public_id, {
+    transformation: [
+      {
+        quality: 'auto',
+        fetch_format:'auto'
+      },
+      {
+        width:1200,
+        height:1200
+      }
+    ]
+  })
+  console.log(url, "cloudinary url obj");
+};
+
+module.exports = { getCloudinaryObj };
